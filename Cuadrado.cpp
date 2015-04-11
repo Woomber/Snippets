9eb4a8ba0c522f1129db59b3c0f81df04575bb06
Woomber <realidadvirtualwm@gmail.com>
2015-04-10 21:37:28 -0500

    Primer commit

---
+++ Cuadrado.cpp
@@ -0,0 +1,28 @@
+#include "stdafx.h"
+#include "iostream"
+
+using namespace std;
+
+void main() {
+	int lados, i, j;
+	cout << "Bienvenido al programa que traza en asteriscos un cuadrado con lado de entre 2 y 20 unidades." << endl << "Creado por Yael Arturo Chavoya Andal\242n." << endl << endl;
+	do {
+		cout << "Inserte una cantidad entre 2 y 20: ";
+		cin >> lados;
+		if (lados<2 & lados >20)
+			cout << "Cantidad fuera de l\241mites. ";
+	} while (lados>=2 & lados <=20);
+	for(i = 0; i<lados; i++){
+		if (i = 0 || i= lados-1){
+			for(j = 0; j<lados;j++)
+				cout << "*";
+		} else {
+			cout<<"*";
+			for(j = 0; j<(lados-2);j++)
+				cout " ";
+		}
+		cout << endl;
+	}
+	system("pause");
+}
+				
